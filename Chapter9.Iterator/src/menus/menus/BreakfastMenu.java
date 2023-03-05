c4fa66b951ef60934bbf99172b2c93fc056a1997
eddbot <edwardsansome@gmail.com>
2023-03-05 16:54:13 +0000

    Grammar/syntax fixes

--- Chapter9.Iterator/src/menus/menus/BreakfastMenu.java
+++ Chapter9.Iterator/src/menus/menus/BreakfastMenu.java
@@ -7,14 +7,14 @@ import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 public class BreakfastMenu {
-    private final ArrayList<MenuItem> menuItems = new ArrayList<MenuItem>();
+    private final ArrayList<MenuItem> menuItems = new ArrayList<>();
 
     public BreakfastMenu() {
 
         IntStream.range(0, 5).forEach(i -> this.menuItems.add(
                         new MenuItem(
-                                "Brekkie item " + i,
-                                "All the same hehe",
+                                "Breakfast item " + i,
+                                "All the same heh",
                                 i,
                                 i % 2 == 0
                         )
